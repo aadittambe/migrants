550f50ba934a00d467ac2db7f88cad669563a716
Aadit Tambe <aadit.tambe@gmail.com>
2022-04-30 01:42:36 -0400

    grunt structure

---
+++ app/Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt){
+
+    // Configuration
+    grunt.initConfig({
+        concat: {
+            js: {
+            //   src: ,
+            //   dest:  ,
+            }
+        }
+        
+    })
+
+    // load plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+
+    // register tasks
+
+};
